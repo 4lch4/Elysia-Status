f121e5a0371dc4d04b297953910f54bb131086b0
Devin W. Leaman <git@4lch4.email>
2023-11-16 13:40:40 -0600

    feat: added first version

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { Elysia } from 'elysia'
+
+export const StatusRoutes = (prefix: string) =>
+  new Elysia({ prefix })
+    .get('/health', () => 'OK', {
+      detail: {
+        description: 'A health check that returns OK by default.',
+        tags: ['Health'],
+        summary: 'Health Check',
+        operationId: 'healthCheck',
+        responses: {
+          200: {
+            description: 'The application is healthy.',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                  example: 'OK',
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+    .get('/readiness', () => 'OK', {
+      detail: {
+        description: 'A readiness check that returns OK by default.',
+        tags: ['Health'],
+        summary: 'Readiness Check',
+        operationId: 'readinessCheck',
+        responses: {
+          200: {
+            description: 'The application is ready to accept requests.',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                  example: 'OK',
+                },
+              },
+            },
+          },
+        },
+      },
+    })
