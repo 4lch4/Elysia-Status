a5d0398d515aee0a80fee26007854e0f53050120
Devin W. Leaman <git@4lch4.email>
2023-11-16 13:45:08 -0600

    refactor: Update optional parameter in StatusRoutes function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from 'elysia'
 
-export const StatusRoutes = (prefix: string) =>
+export const StatusRoutes = (prefix?: string) =>
   new Elysia({ prefix })
     .get('/health', () => 'OK', {
       detail: {
